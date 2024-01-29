c4299e2dcb89221d735685dfc704624d2fc81af1
DorinVieru <dorino.vieru@gmail.com>
2024-01-29 13:44:38 +0100

    [UP] Create a basic structure of files

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> <!-- Collegamento a font awesome -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> <!-- Collegamento a bootstrap -->
+    <link rel="stylesheet" href="./css/style.css"> <!-- Collegamento al file css -->
+    <title>PHP ToDo List JSON</title>
+</head>
+
+<body>
+    <!-- CONTENITORE #APP -->
+    <div id="app">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-primary text-center">PHP ToDo List JSON</h1>
+                </div>
+                <div class="col-12">
+                    <ul class="list-unstyled">
+                        
+                    </ul>
+                </div>
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- SCRIPT JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.2/axios.min.js" integrity="sha512-b94Z6431JyXY14iSXwgzeZurHHRNkLt9d6bAHt7BZT38eqV+GyngIi/tVye4jBKPYQ2lBdRs0glww4fmpuLRwA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> <!-- Collegamento ad axios -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> <!-- Collegamento a vuejs -->
+    <script type="text/javascript" src="./js/script.js"></script> <!-- Collegamento al file js -->
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,23 @@
+// DICHIARO VARIABILE DEL METODO createApp
+const { createApp } = Vue;
+
+// INIZIALIZZAZIONE createApp E CREAZIONE ISTANZA DELL'APPLICAZIONE VUE
+createApp({
+    // DEFINISCO IL METODO data () NEL QUALE INSERIRO' UN RETURN
+    data() {
+        return {
+            
+        }
+    }, // Chiusura data
+
+    mounted() {
+        
+    }, // Chiusura mounted
+
+    // DEFINISCO IL METODO methods {} NEL QUALE INSERIRO' LE FUNZIONI
+    methods: {
+        
+    }, // Chiusura methods
+
+    // CHIUSURA createApp CON .mount("ID")
+}).mount('#app');
